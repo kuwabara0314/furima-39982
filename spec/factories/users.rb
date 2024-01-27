8346aa4b402b964a5e20f606caaf53602ce2e679
kuwabara0314 <148938219+kuwabara0314@users.noreply.github.com>
2024-01-27 21:45:43 +0900

    ユーザー管理機能実装・単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+
+FactoryBot.define do
+  factory :user do
+    nickname                  {Faker::Name.initials(number: 2)}
+    email                     {Faker::Internet.email}
+    password                  {Faker::Internet.password(min_length: 6)} 
+    password_confirmation     {password} 
+    last_name                 {Faker::Name.initials(number: 2)} 
+    first_name                {Faker::Name.initials(number: 2)} 
+    last_name_kana            {Faker::Name.initials(number: 2)}
+    first_name_kana           {Faker::Name.initials(number: 2)} 
+    birthday                  {Faker::Date.birthday(min_age: 20,max_age: 150)}
+  end
+end
