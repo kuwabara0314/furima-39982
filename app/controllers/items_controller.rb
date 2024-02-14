a84c1a13ba94a0c1414f055e1a3c1102815016d9
kuwabara0314 <148938219+kuwabara0314@users.noreply.github.com>
2024-02-14 15:19:56 +0900

    商品情報編集機能（任意修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if user_signed_in? && current_user.id == @item.user_id
-    else 
-      redirect_to root_path
+    if current_user.id != @item.user_id
+       redirect_to root_path
     end
   end
 
