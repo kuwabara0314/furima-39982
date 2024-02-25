c2dcd778cd6aa53658d58258fd88267ae762244b
kuwabara0314 <148938219+kuwabara0314@users.noreply.github.com>
2024-02-25 20:38:49 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
-    binding.pry
     if @item.save
       redirect_to root_path
     else
